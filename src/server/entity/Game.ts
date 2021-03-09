7d59877294263bd32f1f24151ba278a672a267cf
Jackie Huang <jhuang@pdftron.com>
2021-03-09 13:11:43 +0800

    getAllGames api

--- src/server/entity/Game.ts
+++ src/server/entity/Game.ts
@@ -33,43 +33,18 @@ export default class Game extends BaseEntity {
   @Column()
   price: number;
 
-  @Field(() => Float, { nullable: true })
-  get averageRating(): number | null {
-    const count = this.reviews.length;
-    if (count === 0) return null;
-    const sum = this.reviews
-      .map((review) => review.rating)
-      .reduce((a, b) => a + b, 0);
-    return sum / count;
-  }
-
   @Field((type) => [Review])
   @OneToMany(() => Review, (review) => review.game)
   reviews: Review[];
+
+  @Field(() => Float, { nullable: true })
+  get averageRating(): number | null {
+    // const count = this.reviews.length;
+    // if (count === 0) return null;
+    // const sum = this.reviews
+    //   .map((review) => review.rating)
+    //   .reduce((a, b) => a + b, 0);
+    // return sum / count;
+    return 0;
+  }
 }
-
-// @ObjectType()
-// @Entity()
-// export class Game  extends BaseEntity{
-//   @Field()
-//   @PrimaryGeneratedColumn('uuid')
-//   id: string;
-
-//   @Field()
-//   @Column()
-//   title: string;
-
-//   @Field(() => Float)
-//   @Column()
-//   price: number;
-
-//   @Field(() => [Review])
-//   @OneToMany(() => Review, (review) => review.game)
-//   @JoinTable()
-//   reviews: Review[];
-
-//   @Field(() => [User])
-//   @ManyToMany(() => User, (user) => user.gamesInLibrary)
-//   @JoinTable()
-//   users: User[];
-// }
--- src/server/resolvers/gameResolvers.ts
+++ src/server/resolvers/gameResolvers.ts
@@ -2,14 +2,16 @@ import {
   Resolver,
   Query,
   Mutation,
-  ResolverInterface,
   Arg,
   InputType,
   Field,
   Float,
-  ID
+  ID,
+  FieldResolver,
+  Root
 } from 'type-graphql';
 import Game from '../entity/Game';
+import Review from '../entity/Review';
 import { getRepository } from 'typeorm';
 
 @InputType()
@@ -21,17 +23,30 @@ class NewGameInput {
   price: number;
 }
 
-
 @Resolver(() => Game)
 export class GameResolver {
   @Query(() => Game)
-  async game(@Arg('id', type => ID) id: string): Promise<Game | undefined> {
+  async game(@Arg('id', (type) => ID) id: string): Promise<Game | undefined> {
     return await getRepository(Game).findOne(id);
+  }
+
+  @Query(() => [Game])
+  async getAllGames(): Promise<Game[]> {
+    return getRepository(Game).find({ skip: 0, take: 20 });
   }
 
   @Mutation(() => Game)
   async addGame(@Arg('input') input: NewGameInput): Promise<Game> {
     const { title, price } = input;
     return await getRepository(Game).save({ title, price });
+  }
+
+  @FieldResolver()
+  async reviews(@Root() game: Game): Promise<Review[]> {
+    return getRepository(Review).find({
+      where: {
+        gameId: game.id
+      }
+    });
   }
 }
